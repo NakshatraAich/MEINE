2c9405587da4744c6f4357dbda659a114b05aa7c
NakshatraAich <92094885+NakshatraAich@users.noreply.github.com>
2023-10-28 18:42:02 +0530

    Update index.js to include webp files

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,14 +1,14 @@
 import logoHeader from './logo-header.svg'
 import logoFooter from './logo-footer.svg'
 
-import bgImage from './bg.png'
+import bgImage from './bg.webp'
 
 import fb from './fb.svg'
 import ig from './ig.svg'
 import link from './link.svg'
 import x from './x.svg'
 import vert from './vert.svg'
-import rarr from './rarr.png'
+import rarr from './rarr.webp'
 
 export{
     logoHeader,
